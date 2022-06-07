ddd579bbc654331dd293366e93e9a979995e4b49
RuchamiBerenshtein <r9743740@gmail.com>
2022-06-07 15:33:32 +0300

    remove notes

--- js/login.js
+++ js/login.js
@@ -3,8 +3,6 @@ const manager = {
     password: "r1234"
 }
 
-// localStorage.setItem('products', null);
-
 const isManager = (() => {
     if (document.getElementById('userName').value === manager.name
         && document.getElementById('userPassword').value === manager.password) {
